a0ce22a16815e3da15151eaab39816dedda3d6e0
gbellini90 <gbellini90@gmail.com>
2018-11-12 09:03:19 -0500

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,19 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
+
+#prior example 
+# class CreateArtists < ActiveRecord::Migration
+#   def change
+#     create_table :artists do |t|
+#       t.string :name
+#       t.string :genre
+#       t.integer :age
+#       t.string :hometown
+#     end
+#   end
+# end
